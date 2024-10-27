4aef2b10c79d9c3342162664e93b50cc19a52d19
SARANG SHARMA <sarangchamp2004@gmail.com>
2024-10-27 09:45:50 +0530

    initial

--- frontend/src/store/authService.js
+++ frontend/src/store/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${process.env.REACT_APP_API_URL}/api/auth/`;
+const API_URL = "http://15.206.147.253/api/auth/";
 
 
 const register = async (userData) => {
